fcb7c0bad4b24fa0bf1acee7e3ce2ada29813f8f
Lyndon Armitage <lyndon.armitage@gmail.com>
2021-05-01 13:27:38 +0100

    Add rulestring handling & ghost map for rendering

--- main.c
+++ main.c
@@ -5,31 +5,53 @@
 #include <stdio.h>
 #include "gifenc.h"
 #include <string.h>
+#include <ctype.h>
 
 #define WIDTH 800
 #define HEIGHT 600
 #define WRITE_GIF 0
+
+typedef struct parsed_rulestring {
+  int born_count;
+  int survive_count;
+  char *rulestring;
+  char *survive_string;
+  char *born_string;
+  int *survive_numbers;
+  int *born_numbers;
+} parsed_rulestring;
 
 bool **new_grid(int width, int height);
 void delete_grid(bool **grid);
 void randomise_grid(bool **grid, int width, int height);
 
 void init_grid();
-void update(int width, int height, ge_GIF * gif, uint16_t delay);
+void update(int width, int height, parsed_rulestring rules, ge_GIF * gif, uint16_t delay);
 
 int count_neighbours(int x, int y, bool **grid, int width, int height);
-bool test_live_cell(int count);
-bool test_dead_cell(int count);
+
+parsed_rulestring parse_rulestring(char *rulestring);
+void print_rules(parsed_rulestring rules);
+bool test_live_cell(int count, int* survive_numbers, int size);
+bool test_dead_cell(int count, int* born_numbers, int size);
 
 void draw();
 void draw_grid(bool **grid, int width, int height);
 void draw_gif_frame(ge_GIF *gif, int width, int height, uint16_t delay);
 
+char rulestring[] = "B3/S23";
 bool paused = true;
 bool **live_grid = NULL;
 bool **swap_grid = NULL;
+int **ghost_grid = NULL;
+
+int **new_ghost_grid(int width, int height);
+void delete_ghost_grid(int **grid);
 
 int main(int argc, char** args) {
+
+  parsed_rulestring rules = parse_rulestring(rulestring);
+  print_rules(rules);
 
   init_grid();
 
@@ -46,7 +68,7 @@ int main(int argc, char** args) {
   uint16_t delay = 30 /100;
 
   while(!WindowShouldClose()) {
-    update(WIDTH, HEIGHT, gif, delay);
+    update(WIDTH, HEIGHT, rules, gif, delay);
     draw();
   }
 
@@ -55,6 +77,35 @@ int main(int argc, char** args) {
 
   if(gif != NULL) {
     ge_close_gif(gif);
+  }
+
+  if(ghost_grid != NULL) {
+    uint8_t ghost_palette[256 * 3];
+    for(int i = 0; i < 256; i++) {
+      uint8_t value = (uint8_t) i;
+      int index = i * 3;
+      ghost_palette[index] = value;
+      ghost_palette[index+1] = value;
+      ghost_palette[index+2] = value;
+    }
+    int depth = 8;
+    ge_GIF * ghost = ge_new_gif("ghost.gif", WIDTH, HEIGHT, ghost_palette, depth, 0);
+
+
+    uint8_t pixels[WIDTH * HEIGHT];
+    for(int x = 0; x < WIDTH; x ++){
+      for(int y = 0; y < HEIGHT; y ++) {
+        int pos = (y * WIDTH) + x;
+        int value = ghost_grid[x][y];
+        if(value > 255) value = 255;
+        if(value < 0) value = 0;
+        pixels[pos] = value;
+      }
+    }
+    memcpy(ghost->frame, pixels, sizeof(pixels));
+    ge_add_frame(ghost, 0);
+    ge_close_gif(ghost);
+    delete_ghost_grid(ghost_grid);
   }
 
   return 0;
@@ -69,9 +120,10 @@ void init_grid() {
   }
   live_grid = new_grid(WIDTH, HEIGHT);
   swap_grid = new_grid(WIDTH, HEIGHT);
+  //ghost_grid = new_ghost_grid(WIDTH, HEIGHT);
 }
 
-void update(int width, int height, ge_GIF * gif, uint16_t delay) {
+void update(int width, int height, parsed_rulestring rules, ge_GIF * gif, uint16_t delay) {
   
   if(IsKeyPressed(KEY_R)) {
     randomise_grid(live_grid, width, height);
@@ -92,11 +144,20 @@ void update(int width, int height, ge_GIF * gif, uint16_t delay) {
         bool new_state = false;
         int live_neighbours = count_neighbours(x, y, grid, width, height);
         if(grid[x][y]) {
-          new_state = test_live_cell(live_neighbours);
+          new_state = test_live_cell(live_neighbours, rules.survive_numbers, rules.survive_count);
         } else {
-          new_state = test_dead_cell(live_neighbours);
+          new_state = test_dead_cell(live_neighbours, rules.born_numbers, rules.born_count);
         }
         copy[x][y] = new_state;
+        if(ghost_grid != NULL) {
+          if(new_state) {
+            if(ghost_grid[x][y] < 255)
+              ghost_grid[x][y] += 10;
+          } else {
+            if(ghost_grid[x][y] > 0)
+              ghost_grid[x][y] --;
+          }
+        }
       }
     }
 
@@ -137,20 +198,83 @@ int count_neighbours(int x, int y, bool **grid, int width, int height) {
   return count;
 }
 
-bool test_live_cell(int count) {
-  if(count < 2)
-    return false;
-  else if(count >= 2 && count <= 3)
-    return true;
-  else
-    return false;
+
+parsed_rulestring parse_rulestring(char *rulestring) {
+  char *working_string = malloc(strlen(rulestring));
+  strcpy(working_string, rulestring);
+  
+  // handle B/S format
+  char *divided = strtok(working_string, "/");
+  if(divided == NULL) {
+    fprintf(stderr, "Invalid rulestring: %s", rulestring);
+    exit(1);
+  }
+  
+  parsed_rulestring result;
+  result.rulestring = rulestring;
+
+  int token_count = 0;
+  while(divided != NULL && token_count < 2) {
+    char *token = divided;
+    char first = token[0];
+
+    if(first == 'B' || first == 'S') {
+      int * numbers = malloc(sizeof(int) * 8);
+      int number_index = 0;
+      if(numbers == NULL) {
+        fprintf(stderr, "Could not allocate array");
+        exit(1);
+      }
+
+      int len = strlen(token);
+      for(int i = 1; i < len; i ++) {
+        char c = token[i];
+        if(!isdigit(c)) continue;
+        int number = (int) (c - '0');
+        numbers[number_index] = number;
+        number_index ++;
+      }
+
+      if(first == 'B') {
+        result.born_string = token;
+        result.born_numbers = numbers;
+        result.born_count = number_index;
+      } else if (first == 'S') {
+        result.survive_string = token;
+        result.survive_numbers = numbers;
+        result.survive_count = number_index;
+      }
+    }
+
+    
+    token_count ++;
+    divided = strtok(NULL, "/");
+  }
+
+  return result;
 }
 
-bool test_dead_cell(int count) {
-  if(count == 3)
-    return true;
-  else
-    return false;
+void print_rules(parsed_rulestring rules) {
+  printf("Rulestring: %s\n", rules.rulestring);
+  printf("Born string: %s\n", rules.born_string);
+  printf("Surive string: %s\n", rules.survive_string);
+}
+
+bool test_live_cell(int count, int* survive_numbers, int size) {
+  for(int i = 0; i < size; i++) {
+    if(count == survive_numbers[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+bool test_dead_cell(int count, int* born_numbers, int size) {
+  for(int i = 0; i < size; i++) {
+    if(count == born_numbers[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 bool **new_grid(int width, int height) {
@@ -198,8 +322,16 @@ void draw_grid(bool **grid, int width, int height) {
   if(grid == NULL) return;
   for(int x = 0; x < width; x ++) {
     for(int y = 0; y < height; y ++) {
-      if(grid[x][y]) {
-        DrawPixel(x, y, WHITE);
+      if(ghost_grid != NULL) {
+        int val = ghost_grid[x][y];
+        if(val > 255) val = 255;
+        else if(val < 0) val = 0;
+        Color color = (Color) {val, val, val, val};
+          DrawPixel(x, y, color);
+      } else {
+        if(grid[x][y]) {
+          DrawPixel(x, y, WHITE);
+        }
       }
     }
   }
@@ -220,4 +352,30 @@ void draw_gif_frame(ge_GIF *gif, int width, int height, uint16_t delay) {
   }
   memcpy(gif->frame, pixels, sizeof(pixels));
   ge_add_frame(gif, delay);
+}
+
+int **new_ghost_grid(int width, int height) {
+
+  int ** grid = malloc(sizeof(int *) * width);
+  if(grid == NULL) {
+    fprintf(stderr, "Could not init grid");
+    exit(1);
+  }
+  for(int x = 0; x < width; x++) {
+    grid[x] = malloc(sizeof(int) * height);
+    if(grid[x] == NULL) {
+      fprintf(stderr, "Could not init grid");
+      exit(1);
+    }
+    for(int y = 0; y < height; y ++) {
+      grid[x][y] = 0;
+    }
+  }
+  return grid;
+}
+
+void delete_ghost_grid(int **grid) {
+  if(grid == NULL) return;
+  free(grid);
+  grid = NULL;
 }
